187dfb452b7b5cb9b0224fb89ccfcc8e2b3dca3b
sourov jahid <sourov.iqonbd007@gmail.com>
2025-01-29 21:17:39 +0100

    New file

---
+++ problem_6.py
@@ -0,0 +1,32 @@
+"""Write a code to return True if the list’s first and last numbers are the same. If the numbers are different, return False.\
+Given:
+
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+Expected Output:
+
+Given list: [10, 20, 30, 40, 10]
+result is True
+
+numbers_y = [75, 65, 35, 75, 30]
+result is False """
+
+
+
+def first_last_same(numberList):
+    print("Given list:", numberList)
+    
+    first_num = numberList[0]
+    last_num = numberList[-1]
+    
+    if first_num == last_num:
+        return True
+    else:
+        return False
+
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("result is", first_last_same(numbers_y))
+    
